0bcf41e4c642b5c3dcc303fb6a78ee857b4b0545
aistomin <andrej.istomin.ikeen@gmail.com>
2022-05-03 16:06:42 +0200

    test code

---
+++ src/main/java/org/test/Test.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.graphstream.graph.Graph;
+
+/**
+ * Created by aistomin on 03.05.22.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("Import works: %s", Graph.class);
+    }
+}
